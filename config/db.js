3fa1b77289e883943d34c163640db757b3adaf20
ke4tri <ke4tri@gmail.com>
2019-09-01 20:11:37 -0500

    added more files and configured to connect to the mongoDB db with mongoose

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI"); 
+
+//To connect to mongoDB use:
+//mongoose.connect(db)
+//or
+//async/await 
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        })
+        console.log("MongoDB Connected...")
+    } catch(err){
+        console.error(err.message);
+        //Exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB; 
