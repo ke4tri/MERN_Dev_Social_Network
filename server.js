685de58f8f37a722ef180d2346ad0728ab87f7c6
ke4tri <ke4tri@gmail.com>
2019-09-01 19:44:58 -0500

    scripts and server populated

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+//If there is no PORT env set it will default to port 5000
+const PORT = process.env.PORT || 5000;
+
+//templet litteral 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
