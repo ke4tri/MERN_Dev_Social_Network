efbc3feb11ad8130fdd4bfa9b29810db20f80ee0
ke4tri <ke4tri@gmail.com>
2019-09-02 10:47:49 -0500

    created routes on the backend and files

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+//Define Routes
+//require points to the server file that has route
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
 //If there is no PORT env set it will default to port 5000
 const PORT = process.env.PORT || 5000;
 
