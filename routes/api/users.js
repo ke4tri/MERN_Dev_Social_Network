d8f761fa1bc5d77023bc46dbbfcdaf3ef10de784
ke4tri <ke4tri@gmail.com>
2019-09-07 07:48:38 -0500

    jwt token returning on user register

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -59,12 +62,31 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send("User registered");
+      // res.send("User registered");
+      //sub res.send with payload
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
     }
   }
 );
+
+router.get("/", (req, res) => res.send("Profile user"));
 
 module.exports = router;
